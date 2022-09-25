b2b919f58e86263ca708d962d2f903a5e8ba22eb
ROBOT\pavel <pavelpetrochenko2001@gmail.com>
2022-09-25 19:52:59 +0300

    update api for IGraph and partialy implementation

--- Runtime/ALGraph.cs
+++ Runtime/ALGraph.cs
@@ -8,8 +8,8 @@ namespace JoyTeam.Core
     public class ALGraph : IGraph
     {
         private int _nodes;
-        private float[] _weight;
-        private List<int>[] _edges;
+        private List<float> _weight;
+        private List<List<int>> _edges;
 
         public ALGraph(int nodes, float defaultWeight = 1.0f)
         {
@@ -20,24 +20,40 @@ namespace JoyTeam.Core
             }
         }
 
+        public void AddNode(int node)
+        {
+            this._edges[node] = new List<int>();
+            this._nodes++;
+        }
+
+        public void RemoveNode(int node)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public void AddEdge(int from, int to)
         {
             this._edges[from].Add(to);
         }
 
-        public List<int> GetNeightbours(int node)
+        public void RemoveEdge(int from, int to)
         {
-            return this._edges[node];
+            throw new System.NotImplementedException();
+        }
+
+        public void SetWeight(int node, float weight)
+        {
+            this._weight[node] = weight;
         }
 
         public float GetWeight(int node)
         {
             return this._weight[node];
         }
-
-        public void SetWeight(int node, float weight)
+        
+        public List<int> GetNeightbours(int node)
         {
-            this._weight[node] = weight;
+            return this._edges[node];
         }
     }
 }
--- Runtime/IGraph.cs
+++ Runtime/IGraph.cs
@@ -4,7 +4,10 @@ namespace JoyTeam.Core
 {
     public interface IGraph
     {
+        public void AddNode(int node);
+        public void RemoveNode(int node);
         public void AddEdge(int from, int to);
+        public void RemoveEdge(int from, int to);
         public void SetWeight(int node, float weight);
         public float GetWeight(int node);
         public List<int> GetNeightbours(int node);
