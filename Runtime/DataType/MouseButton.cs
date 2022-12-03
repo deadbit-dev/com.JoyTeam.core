3991112f9862a3c5c89fba7dd69aead9a3f84f50
ROBOT\pavel <pavelpetrochenko2001@gmail.com>
2022-12-03 16:23:11 +0300

    added MouseButton DataType

---
+++ Runtime/DataType/MouseButton.cs
@@ -0,0 +1,13 @@
+namespace JoyTeam.Core.DataType
+{
+    public enum MouseButton
+    {
+        Left = 323, // start index mouse buttons in UnityEngine.KeyCode 
+        Right,
+        Middle,
+        AFourth,
+        AFifth,
+        ASixth,
+        ASeventh
+    }
+}
